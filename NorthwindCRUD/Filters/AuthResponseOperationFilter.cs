96e93916d440aec972e94cab861390a48d7241f5
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2022-11-17 16:08:51 +0200

    fix(swagger): remove lock icon for anonymous users

---
+++ NorthwindCRUD/Filters/AuthResponseOperationFilter.cs
@@ -0,0 +1,39 @@
+﻿namespace NorthwindCRUD.Filters
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.OpenApi.Models;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+    public class AuthResponsesOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var authAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AuthorizeAttribute>();
+
+            if (authAttributes.Any())
+            {
+                var securityRequirement = new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                }
+            };
+                operation.Security = new List<OpenApiSecurityRequirement> { securityRequirement };
+                operation.Responses.Add("401", new OpenApiResponse { Description = "Unauthorized" });
+            }
+        }
+    }
+}
