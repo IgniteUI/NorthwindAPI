1c6e9df36f9f70b3607df9a055b5bf2ffebbd640
Dimitar Georgiev Dimitrov <DGDimitrov@infragistics.com>
2024-10-10 16:00:15 +0300

    fix(*): phone numbers regex fix

--- NorthwindCRUD/Models/Dtos/ShipperDto.cs
+++ NorthwindCRUD/Models/Dtos/ShipperDto.cs
@@ -11,7 +11,7 @@ namespace NorthwindCRUD.Models.Dtos
         [StringLength(100, ErrorMessage = "Company Name cannot exceed 100 characters.")]
         public string CompanyName { get; set; }
 
-        [RegularExpression(@"^\+?[1-9]\d{1,14}$", ErrorMessage = "Phone number is not valid.")]
+        [RegularExpression(@"^\+?\(?\d{1,5}\)?[-.\s]?\(?\d{1,5}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,10}$", ErrorMessage = "Phone number is not valid.")]
         public string Phone { get; set; }
     }
 }
