245d062ce2f21d9a342bac4fe23e4ce90a7149fd
Javier Coitino <jcoitino@infragistics.com>
2024-10-19 11:36:54 -0300

    code polishing

--- NorthwindCRUD/Controllers/QueryBuilderController.cs
+++ NorthwindCRUD/Controllers/QueryBuilderController.cs
@@ -13,17 +13,17 @@ using System.Globalization;
 [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1011:Closing square brackets should be spaced correctly", Justification = "...")]
 public class QueryBuilderResult
 {
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public AddressDto[]? Addresses { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public CategoryDto[]? Categories { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public ProductDto[]? Products { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public RegionDto[]? Regions { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public TerritoryDto[]? Territories { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public EmployeeDto[]? Employees { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public CustomerDto[]? Customers { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public OrderDto[]? Orders { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public AddressDto[]?     Addresses    { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public CategoryDto[]?    Categories   { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public ProductDto[]?     Products     { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public RegionDto[]?      Regions      { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public TerritoryDto[]?   Territories  { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public EmployeeDto[]?    Employees    { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public CustomerDto[]?    Customers    { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public OrderDto[]?       Orders       { get; set; }
     [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public OrderDetailDto[]? OrderDetails { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public ShipperDto[]? Shippers { get; set; }
-    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public SupplierDto[]? Suppliers { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public ShipperDto[]?     Shippers     { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)] public SupplierDto[]?    Suppliers    { get; set; }
 }
 
 [ApiController]
