b14b08888802f4558c4989b16dc341bc74b705db
pmoleri <pmoleri@infragistics.com>
2025-03-28 10:20:51 -0300

    Remove typo
    Co-authored-by: Zdravko Kolev <zdrawko.kolew@gmail.com>

--- NorthwindCRUD/Controllers/QueryBuilderController.cs
+++ NorthwindCRUD/Controllers/QueryBuilderController.cs
@@ -67,7 +67,7 @@ public class QueryBuilderController : ControllerBase
                     "orderdetails" => dataContext.OrderDetails.Run<OrderDetailDb, OrderDetailDto>(query, mapper),
                     "shippers" => dataContext.Shippers.Run<ShipperDb, ShipperDto>(query, mapper),
                     "suppliers" => dataContext.Suppliers.Run<SupplierDb, SupplierDto>(query, mapper),
-                    _ => throw new InvalidOperationException($"Unknown entity ${t}"),
+                    _ => throw new InvalidOperationException($"Unknown entity {t}"),
                 }
             },
         });
