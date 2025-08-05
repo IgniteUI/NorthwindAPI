e5053e0bce87810d1af70bfdcfb4bc50e1c282d2
Zdravko Kolev <zdrawko.kolew@gmail.com>
2025-08-05 11:25:53 +0300

    Revert "Add Require attribute tofields in AddressDto"

--- NorthwindCRUD/Models/Dtos/AddressDto.cs
+++ NorthwindCRUD/Models/Dtos/AddressDto.cs
@@ -5,19 +5,15 @@ namespace NorthwindCRUD.Models.Dtos
 {
     public class AddressDto : IAddress
     {
-        [Required(ErrorMessage = "Street is required.")]
         [StringLength(100, ErrorMessage = "Street cannot exceed 100 characters.")]
         public string Street { get; set; }
 
-        [Required(ErrorMessage = "City is required.")]
         [StringLength(50, ErrorMessage = "City cannot exceed 50 characters.")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Region is required.")]
         [StringLength(50, ErrorMessage = "Region cannot exceed 50 characters.")]
         public string Region { get; set; }
 
-        [Required(ErrorMessage = "PostalCode is required.")]
         [StringLength(20, ErrorMessage = "Postal code cannot exceed 20 characters.")]
         public string PostalCode { get; set; }
 
